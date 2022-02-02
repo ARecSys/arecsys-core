308dee7d024f973a4eead337e9b98b78bacfa31b
romain.ducret <romain.ducret@student-cs.fr>
2022-02-02 15:32:18 +0100

    pagerank basic

--- src/app.py
+++ src/app.py
@@ -11,3 +11,9 @@ def get_rec_from_doi( doi ):
     res = algofst( [ doi ] )
 
     return jsonify( [ art.toJson() for art in res ] )
+
+app.run(host='0.0.0.0', port=8080)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
